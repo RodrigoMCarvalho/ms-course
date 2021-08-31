dd8a55181b3aa05d2d0e83e655981d5fa76b7f78
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-08-31 19:54:52 -0300

    JWT token

--- hr-oauth/src/main/java/com/rodrigo/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/rodrigo/hroauth/services/UserService.java
@@ -6,10 +6,13 @@ import com.rodrigo.hroauth.feignClient.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -20,6 +23,15 @@ public class UserService {
         User user = userFeignClient.findByEmail(email).getBody();
         if(user == null) {
             throw new EmailNotFountException("Email não encontrado");
+        }
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if(user == null) {
+            throw new UsernameNotFoundException("Usuario não encontrado");
         }
         return user;
     }
